cf1bf7f1b8e23967bf6ba0a56543f4353f8e8fdf
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-09-19 20:22:31 -0300

    component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from './components/Header';
 import Globals from './styles/global'
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Globals />
-    </div>
+    </>
   );
 }
 
